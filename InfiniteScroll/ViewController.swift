ee98c6eba497625360640de16f4436b95f483bcf
Karthikeyan <karthikeyan.bose@cognizant.com>
2023-09-12 22:37:26 +0530

    Initial Commit

---
+++ InfiniteScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteScroll
+//
+//  Created by Bose, Karthikeyan (Cognizant) on 12/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
